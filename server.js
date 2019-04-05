bf64f73f535451d7fbe936f22f5673350ea75513
Dan Norman <normande@pkcsecurity.com>
2019-04-05 14:24:05 -0700

    to mongodb

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 var cors = require('cors')
 const app = express();
 var bodyParser = require('body-parser')
+var MongoClient = require('mongodb').MongoClient
 
 app.use(cors())
 app.use( bodyParser.json() );
@@ -12,10 +13,25 @@ app.get('/', function (req, res) {
 })
 
 app.post('/save', function (req, res) {
-    console.log ('here is the req ');
-    console.log (req);
-    var state = req.body.state;
+    var state = req.body;
+    console.log (state);
     res.send('saved it ' + state);
+  MongoClient.connect('mongodb://localhost:27017/tik', function (err, client) {
+   if (err) throw err
+
+  var db = client.db('tik')
+ console.log ("1"); 
+  db.collection('tik').insertOne( { state : state } );
+
+ console.log ("2");
+  db.collection('tik').find().toArray(function (err, result) {
+    if (err) throw err
+
+    console.log(result)
+  }) 
+})
+
+
 });
 
 app.set('port', 8080);
